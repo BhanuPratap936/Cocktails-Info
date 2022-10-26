d872b732841220882184890ac56b3d194df5d97a
Bhanu <bp852109@gmail.com>
2022-10-26 15:33:40 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quisquam
+				quis odit neque impedit, suscipit fuga, voluptate incidunt repudiandae
+				earum consequatur sunt repellendus. Molestiae vitae quibusdam
+				exercitationem, ullam illo eligendi.
+			</p>
+		</section>
+	);
+};
+
+export default About;
